6435569d2b697198528d534f71c953feba094cb5
JesseAbram <jesseabramowitz@hotmail.com>
2020-08-11 09:38:49 -0400

    adds a pair function

--- pallets/uniswap/src/tests.rs
+++ pallets/uniswap/src/tests.rs
@@ -2,22 +2,22 @@ use crate::{Error, mock::*};
 use frame_support::{assert_ok, assert_noop};
 
 #[test]
-fn it_works_for_default_value() {
+fn it_adds_a_pair() {
 	new_test_ext().execute_with(|| {
 		// Dispatch a signed extrinsic.
-		assert_ok!(Uniswap::do_something(Origin::signed(1), 42));
+		assert_ok!(Uniswap::add_pair(Origin::signed(1), 42));
 		// Read pallet storage and assert an expected result.
-		assert_eq!(Uniswap::something(), Some(42));
+		assert_eq!(Uniswap::token_balances(42), 0);
 	});
 }
 
-#[test]
-fn correct_error_for_none_value() {
-	new_test_ext().execute_with(|| {
-		// Ensure the expected error is thrown when no value is present.
-		assert_noop!(
-			Uniswap::cause_error(Origin::signed(1)),
-			Error::<Test>::NoneValue
-		);
-	});
-}
+// #[test]
+// // fn correct_error_for_none_value() {
+// // 	new_test_ext().execute_with(|| {
+// // 		// Ensure the expected error is thrown when no value is present.
+// // 		assert_noop!(
+// // 			Uniswap::cause_error(Origin::signed(1)),
+// // 			Error::<Test>::NoneValue
+// // 		);
+// // 	});
+// }
